9e6823d809e4c769f87b35d614f28d3e79ce8fc2
Infowaygit <124226931+Infowaygit@users.noreply.github.com>
2023-02-02 09:55:41 +0530

    Add files via upload

---
+++ github.java
@@ -0,0 +1,18 @@
+
+public class github {
+
+    public static int add(int a , int b)
+    {
+        return a+b;
+    }
+
+
+    public static void main(String[] args) {
+
+    int a =github.add(5,   9);
+    System.out.println(a);
+        
+    }
+    
+}
+
